a69eac22c0be256dad1105af555ed7cc7007aad0
r4korompis <69853617+r4korompis@users.noreply.github.com>
2021-03-19 09:52:11 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
-const humburgerMenu = document.querySelector(".humburger-menu input");
+const humburgerMenuInput = document.querySelector(".humburger-menu input");
 const slide = document.querySelector(".slide");
+const link = document.querySelectorAll(".slide li");
+const container = document.querySelector(".container");
 
-humburgerMenu.addEventListener("click", function () {
+humburgerMenuInput.addEventListener("click", function () {
   slide.classList.toggle("slide-open");
+});
+
+link.forEach(function (e) {
+  e.addEventListener("click", function () {
+    slide.classList.toggle("slide-open");
+    humburgerMenuInput.checked = false;
+  });
+});
+
+container.addEventListener("click", function () {
+  if (humburgerMenuInput.checked == true) {
+    slide.classList.toggle("slide-open");
+    humburgerMenuInput.checked = false;
+  }
 });
