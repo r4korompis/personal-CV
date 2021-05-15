258e4a3abc323156d2aa38980d5e98a5893a0b7d
r4korompis <r4korompis@gmail.com>
2021-05-15 13:05:15 +0800

    menambahkan fitur kirim pesan lewat contact form.

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const humburgerMenuInput = document.querySelector(".humburger-menu input");
 const slide = document.querySelector(".slide");
 const link = document.querySelectorAll(".slide li");
 const container = document.querySelector(".container");
+const closeIcon = document.querySelector(".closeIcon");
+const myAlert = document.querySelector(".myalert");
 
 humburgerMenuInput.addEventListener("click", function () {
   slide.classList.toggle("slide-open");
@@ -19,4 +21,8 @@ container.addEventListener("click", function () {
     slide.classList.toggle("slide-open");
     humburgerMenuInput.checked = false;
   }
+});
+
+closeIcon.addEventListener("click", function () {
+  myAlert.classList.toggle("none");
 });
