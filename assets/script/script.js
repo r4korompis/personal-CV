2c85a5866d28a8b555e9f7fe96d38d857bbb4e6c
ranggakorompis <korompisrangga14@gmail.com>
2023-01-11 14:01:26 +0800

    delete aos package

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,8 +1,10 @@
 const humburger = document.querySelector(".humburger-menu span");
 const nav = document.querySelector(".nav-box");
+// const closeIcon = document.getElementById("close-icon");
 const humburgerMenu = () => {
   nav.classList.toggle("toggle");
   humburger.classList.toggle("fixed");
+  // closeIcon.setAttribute("src", "assets/icons/close-icon.svg");
 };
 humburger.addEventListener("click", humburgerMenu);
 
