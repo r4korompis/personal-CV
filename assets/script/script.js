ba3bfb47d3f8b6083d4a19a9b3ac6937caad831d
ranggakorompis <korompisrangga14@gmail.com>
2023-01-12 08:01:23 +0800

    delete arrow up button

--- assets/script/script.js
+++ assets/script/script.js
@@ -7,19 +7,3 @@ const humburgerMenu = () => {
   // closeIcon.setAttribute("src", "assets/icons/close-icon.svg");
 };
 humburger.addEventListener("click", humburgerMenu);
-
-const btnScrollTop = document.getElementById("btnScrollTop");
-window.onscroll = () => {
-  scrollFunction();
-};
-const scrollFunction = () => {
-  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-    btnScrollTop.style.display = "block";
-  } else {
-    btnScrollTop.style.display = "none";
-  }
-};
-btnScrollTop.addEventListener("click", function () {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
-});
